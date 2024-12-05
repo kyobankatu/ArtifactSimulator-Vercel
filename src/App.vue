8dde9edb92375fcc764c30b65bd7b670623b12d4
katumon <kentokentokento601@gmail.com>
2024-12-05 14:31:32 +0900

    first commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app" style="text-align: center; margin-top: 50px;">
+    <h1>Hello, World!</h1>
+    <p>Welcome to my Vue.js app!</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style scoped>
+/* Add any styles you want here */
+</style>
